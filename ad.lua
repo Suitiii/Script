eecc66e6c89f3ba3e2c2577d40b63ec6cd9210c9
Suitiii <115899831+Suitiii@users.noreply.github.com>
2024-06-10 01:47:44 -0300

    Update ad.lua

--- ad.lua
+++ ad.lua
@@ -32,7 +32,7 @@ local gui = Instance.new("ScreenGui")
 	velocidade.Parent = frame
 	
 	gui.Parent = player.PlayerGui
-	
+	frame.ZIndex = 10
 	frame.Transparency = 1
 	text.Transparency = 1
 	velocidade.Transparency = 1
