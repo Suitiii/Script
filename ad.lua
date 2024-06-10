3f06b2489c5c0e67145d9cce908f6a744854a7b9
Suitiii <115899831+Suitiii@users.noreply.github.com>
2024-06-10 01:10:41 -0300

    Update ad.lua

--- ad.lua
+++ ad.lua
@@ -1,1 +1,20 @@
-lua sla oq
+
+local player = game.Players.LocalPlayer
+
+local gui = Instance.new("ScreenGui")
+    local text = Instance.new("TextLabel")
+    text.Text = "Hack By Lula"
+    text.Position = UDim2.new(0.309, 0,0.061, 0)
+    text.Size = UDim2.new(0.384, 0,0.1, 0)
+    text.TextScaled = true
+    
+    local frame = Instance.new("Frame")
+    local round = Instance.new("UICorner")
+    text.Parent = frame
+    round.Parent = frame
+    frame.Position = UDim2.new(0.279, 0,0.236, 0)
+    frame.Size = UDim2.new(0.441, 0,0.525, 0)
+    frame.Parent = gui
+    gui.Parent = player.PlayerGui
+    
+    
