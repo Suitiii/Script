9558072b81bfb13eadf53fc069c3dab04d3db37c
Suitiii <115899831+Suitiii@users.noreply.github.com>
2024-07-01 22:55:34 -0300

    Update ScriptYBA.lua

--- ScriptYBA.lua
+++ ScriptYBA.lua
@@ -1,2 +1,20 @@
-print("bola")
+local works = game:GetService("Workspace")
 
+local folder1 = works:WaitForChild("Item_Spawns")
+
+local folder2 = folder1:WaitForChild("Items")
+
+
+
+if works then
+  print(1)
+end
+
+
+if folder1 then
+  print(2)
+end
+
+if folder2 then 
+  print(3)
+end
